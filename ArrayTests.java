c72a7b50581ed16da6e05e5ab6c49e7cd123a7d5
emmaz12 <ezc7288@gmail.com>
2023-01-26 14:46:27 -0800

    update
    update

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,13 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input2);
+
+    //test passes
+    assertArrayEquals("demonstrating that the actual output is incorrect", 
+        input2, new int[]{5,4,3,2,1});
 	}
 
 
@@ -14,5 +21,10 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {1,2,3,4,5};
+    assertArrayEquals(ArrayExamples.reversed(input2), new int[] {5,4,3,2,1});
   }
+
+  
 }
